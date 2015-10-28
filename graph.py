645b04b5a3309a214d7b1992ae6c96565f7cae3b
EPodesta <epodesta158@gmail.com>
2015-10-28 21:08:59 -0200

    Little documentation added

--- graph.py
+++ graph.py
@@ -1,141 +1,161 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
 import random
-class Grafo(object):
+#Essa classe será responsável por representar um grafo, possuindo as funções
+#básicas para sua representação.
+class Graph(object):
 
-    def __init__(self, verticeInicial, weight=False, orientado=False):
-        self._ordem = 0
-        self.grafo = defaultdict(dict)
-        self._orientado = orientado
+#Inicializa o grafo. O init recebe como parâmetro o vértice inicial, um boolean
+#para determinar se possui peso ou não e outro boolean para determinar se o grafo
+#será orientado ou não. A base para esse grafo foi um Dicionário de Dicionários.
+    def __init__(self, initialVertex, weight=False, oriented=False):
+        self.graph = defaultdict(dict)
+        self._oriented = oriented
         self._weight = weight
-        self.adicionarVertice(verticeInicial)
+        self.addVertex(initialVertex)
 
-    def adicionarVertice(self, v1):
-        self.grafo[v1]
-        self._ordem += 1
+#Esse método adicionará um vértice no grafo(dicionário).
+    def addVertex(self, v1):
+        self.graph[v1]
 
-#conecta
-    def adicionarAresta(self, v1, v2, weight=0):
+#O método addEdge irá conectar dois vértices e, caso tenha sido especificado que o grafo
+#será valorado, atribuir um peso.
+    def addEdge(self, v1, v2, weight=0):
 
-        if not self._orientado:
+        if not self._oriented:
             if self._weight:
-                self.grafo[v1][v2] = weight
-                self.grafo[v2][v1] = weight
+                self.graph[v1][v2] = weight
+                self.graph[v2][v1] = weight
     
             else:
     
-                self.grafo[v1][v2]
-                self.grafo[v2][v1]
+                self.graph[v1][v2]
+                self.graph[v2][v1]
         else:
             if self._weight:
-                self.grafo[v1][v2] = weight
+                self.graph[v1][v2] = weight
 
             else:
-                self.grafo[v1][v2]
+                self.graph[v1][v2]
 
-    def removerVertice(self, v):
-        for vertice, adjacentes in self.grafo.items():
+    def deleteVertex(self, v):
+        for vertex, adjacent in self.graph.items():
             try:
-               adjacentes.pop(v, None)
+               adjacent.pop(v, None)
             except KeyError:
                 pass
         try:
-            del self.grafo[v]
+            del self.graph[v]
         except KeyError:
             pass
-        self._ordem -= 1
 
-#desconecta
-    def removerAresta(self, v1, v2):
-        if not self._orientado:
+#O método deleteEdge desconectará dois vértices.
+    def deleteEdge(self, v1, v2):
+        if not self._oriented:
             try:
-                del self.grafo[v1][v2]
+                del self.graph[v1][v2]
             except KeyError:
                 pass
             try:
-                del self.grafo[v2][v1]
+                del self.graph[v2][v1]
             except KeyError:
                 pass
         else:
             try:
-                del self.grafo[v1][v2]
+                del self.graph[v1][v2]
             except KeyError:
                 pass 
+#O método connected verificará se dois vértices estão conectados em um grafo.
+    def connected(self, v1, v2):
 
-    def conectado(self, v1, v2):
-
-        if v2 in self.grafo[v1]:
+        if v2 in self.graph[v1]:
             return True
         else:
             return False
 
-    def ordem(self):
-        return self._ordem
+#O método order retornará a ordem do Grafo(Número de vértices).
+    def order(self):
+        return len(self.graph)
 
-    def vertices(self):
-        conjunto = set()
-        for v, l in self.grafo.items():
-            conjunto.add(v)
-        return conjunto
+#O método vertexes irá retornar um set com todos os vértices do Grafo.
+    def vertexes(self):
+        _set = set()
+        for v, l in self.graph.items():
+            _set.add(v)
+        return _set
 
-    def umVertice(self):
-        rand = random.randrange(1, self._ordem)
+#OneVertex coletará um vértice aleatório do grafo.
+    def oneVertex(self):
+        order = self.order()
+        rand = random.randrange(1, order)
         count = 0
-        for v, l in self.grafo.items():
+        for v, l in self.graph.items():
             if count == rand:
                 return v
             count += 1
 
-    def adjacentes(self, v):
-        adj = self.grafo[v]
-        conjunto = set()
-        for vertices in adj:
-            conjunto.add(vertices)
-        return conjunto
+#O método adjacent retornará um set com todos os vértices adjacentes a outro vértice.
+    def adjacent(self, v):
+        adj = self.graph[v]
+        _set = set()
+        for vertex in adj:
+            _set.add(vertex)
+        return _set
 
-    def grauSucessores(self, v):
-        return self.grafo[v]
+#O método successor será responsável por coletar os sucessores de um vértice em um grafo
+#orientado.
+    def successor(self, v):
+        if self._oriented:
+            return self.graph[v]
+        else:
+            print("Successor only makes sense in a oriented graph!")
 
-    def grauAntecessores(self, v):
-        return {i for i in self.grafo if v in self.grafo[i]}
+#O método predecessor retornará os predecessores de um dado vértice em um grafo orientado.
+    def predecessor(self, v):
+        if self._oriented:
+            return {i for i in self.graph if v in self.graph[i]}
+        else:
+            print("Predecessor only makes sense in a oriented graph!")
 
-
-    def grau(self, v):
-        adj = self.adjacentes(v)
+#O método degree retornará a quantidade de arestas de um dado vértice, ou seja, seu grau.
+    def degree(self, v):
+        adj = self.adjacent(v)
         count = 0
-        for vertices in adj:
+        for vertexes in adj:
             count += 1
         return count
 
-    def peso(self, v1, v2):
-        return self.grafo[v1][v2]
+#O método weight retornará o peso de uma aresta, sendo especificada pelos vértices que ela conecta.
+    def weight(self, v1, v2):
+        return self.graph[v1][v2]
 
-    def buscaProfundidade(self, v, vA, jaVisitados, l):
-        if v in jaVisitados:
+#Os métodos depthFirstSearch e searchCycle farão uma busca por profundidade no grafo,
+#seguindo como objetivo verificar se há ciclos no grafo.
+    def depthFirstSearch(self, v, vA, alreadyVisited, l):
+        if v in alreadyVisited:
             return True
-        jaVisitados.add(v)
+        alreadyVisited.add(v)
         if vA != None:
             l.remove(v)
-        for x in self.adjacentes(v):
+        for x in self.adjacent(v):
             if x != vA:
-                if self.buscaProfundidade(x, v, jaVisitados, l):
+                if self.depthFirstSearch(x, v, alreadyVisited, l):
                     return True
         return False
-# def buscaLargura():
 
-
-    def buscaCiclo(self, v):
-        l = self.vertices()
-        lista = set()
+    def searchCycle(self, v):
+        l = self.vertexes()
+        _list = set()
         while True:
             try:
                 x = l.pop()
             except KeyError:
                 break
-            if self.buscaProfundidade(x, None, lista,l):
+            if self.depthFirstSearch(x, None, _list,l):
                 return True
 
         return False
-
+        
+#Método determinado para "printar" o grafo.
     def __str__(self):
-        return '{}({})'.format(self.__class__.__name__, dict(self.peso))
+        return '{}({})'.format(self.__class__.__name__, dict(self.weight))
